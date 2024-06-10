2f9ef0805603f5d1fb5d985581519b1cae5dbf96
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-10 00:02:27 -0500

    figuring out how to do the light/dark mode toggle

---
+++ assets/js/form.js
@@ -0,0 +1,17 @@
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+let mode = 'dark'
+
+themeSwitcher.addEventListener('click', function(event) {
+    if (mode === 'dark') {
+        mode = 'light';
+    container.setAttribute('class','light');
+}
+
+else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+}
+});
+
