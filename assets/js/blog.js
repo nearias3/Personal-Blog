0f5c14afa8472d05575391d659bc08e47af9480d
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-10 00:59:56 -0500

    set up basic css style for form and comments for future changes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,18 @@
-// add event listener for light/dark mode toggle
 // back button sends user back to the the homepage
 // blogs are added from each submit into the containers
+
+
+const themeSwitcher = document.getElementById("modeToggle");
+const container = document.body;
+
+let mode = "dark";
+
+themeSwitcher.addEventListener("click", function () {
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  } else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
