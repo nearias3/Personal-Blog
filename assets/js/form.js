a4b91bc39cc4f3728382157db74258dd02427680
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-10 00:50:33 -0500

    Set up toggle modes in form html and style css

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,22 @@
+const themeSwitcher = document.getElementById('modeToggle');
+const container = document.body;
+
+let mode = 'dark';
+
+themeSwitcher.addEventListener("click", function () {
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
+
+
+
 // prevent default event
 // set up form values
 // set up error message for form
 // how to store blogs in local storage???
-// add event listener for light/dark mode toggle
