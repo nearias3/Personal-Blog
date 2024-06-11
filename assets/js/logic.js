1204e80d9afffa7d0501eab2d3d8f86d439b9669
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-10 19:38:34 -0500

    Added code to logic for storing and retrieving data, not sure it works just yet!

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,14 @@
-// how to store blog post info in localstorage
+function storeBlogPost(username, title, content) {
+    const blogPost = { username, title, content };
+    let blogPosts = JSON.parse(localStorage.getItem('blogPosts'));
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+}
+
+function getBlogPosts() {
+    return JSON.parse(localStorage.getItem('blogPosts'))
+}
+
+export { storeBlogPost, getBlogPosts, validateFormInputs}
 
 // how to retrieve all the blog posts from localstorage
 
